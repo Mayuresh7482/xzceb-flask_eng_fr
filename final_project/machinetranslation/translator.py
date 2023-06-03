acf60a7d939a677ea8bf2c4103ea24c880aeae93
Mayuresh Borate <119850489+Mayuresh7482@users.noreply.github.com>
2023-06-03 18:52:48 +0530

    Update translator.py

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,44 +1,17 @@
 from deep_translator import MyMemoryTranslator
 
-
-def englishToFrench(english_text):
+def english_to_french(english_text):
+    #write the code here
     """
-    Translates English text to French using MyMemoryTranslator.
-    Args:
-        english_text (str): The English text to be translated.
-    Returns:
-        str: The translated French text.
+    This function returns the traduction of english text to french
     """
-    translator = MyMemoryTranslator(source='en', target='fr')
-    french_text = translator.translate(english_text)
+    french_text = MyMemoryTranslator(source='en', target='fr').translate(english_text)
     return french_text
 
-
-def frenchToEnglish(french_text):
+def french_to_english(french_text):
+    #write the code here
     """
-    Translates French text to English using MyMemoryTranslator.
-    Args:
-        french_text (str): The French text to be translated.
-    Returns:
-        str: The translated English text.
+    This function returns the traduction of french text to english
     """
-    translator = MyMemoryTranslator(source='fr', target='en')
-    english_text = translator.translate(french_text)
+    english_text= MyMemoryTranslator(source='fr', target='en').translate(french_text)
     return english_text
-
-
-def translate_english_to_french():
-    english_text = input("Enter an English word or phrase: ")
-    french_translation = englishToFrench(english_text)
-    print(f"The French translation of '{english_text}' is '{french_translation}'.")
-
-
-def translate_french_to_english():
-    french_text = input("Enter a French word or phrase: ")
-    english_translation = frenchToEnglish(french_text)
-    print(f"The English translation of '{french_text}' is '{english_translation}'.")
-
-
-if __name__ == '__main__':
-    translate_english_to_french()
-    translate_french_to_english()
